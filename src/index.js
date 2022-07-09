07601f65d0d3abc355996d37222f26917293e87c
schwarzwald0906 <93188453+schwarzwald0906@users.noreply.github.com>
2022-07-09 19:54:11 +0900

    JavaScript 基礎の学習

--- src/index.js
+++ src/index.js
@@ -44,3 +44,63 @@
 // val5[0] = "bird";
 // val5.push("monkey");
 // console.log(val5);
+
+// const name = "じゃけえ";
+// const age = 28;
+
+// const message1 = "私の名前は" + name + "です。年齢は" + age + "歳です。";
+// console.log(message1);
+
+// const message2 = `My name is ${name}. My age is ${age}.`;
+// console.log(message2);
+
+/**
+ * アロー関数
+ */
+
+//従来の書き方
+// function func1(str){
+//   return str;
+// }
+
+// const func1 = function (str) {
+//   return str;
+// };
+
+// console.log(func1("func1です"));
+
+//arrow関数
+// const func2 = (str) => {
+//   return str;
+// };
+// const func2 = str => {
+//   return str;
+// };
+// const func2 = (str) => str;
+// console.log(func2("func2です"));
+
+// const func3 = (num1,num2) =>{
+//   return num1 + num2;
+// }
+
+// console.log(func3(20, 30));
+
+// const myProfile = {
+//   name: "じゃけえ",
+//   age: 28
+// };
+
+// const message1 = `私の名前は${myProfile.name}です。年齢は${myProfile.age}です。`;
+// console.log(message1);
+
+// const { name, age } = myProfile;
+// const message2 = `私の名前は${name}です。年齢は${age}です。`;
+// console.log(message2);
+
+// const myProfile = [`じゃけえ`, 28];
+// const message3 = `名前は${myProfile[0]}です。年齢は${myProfile[1]}です`;
+// console.log(message3);
+
+// const [name , age ] = myProfile;
+// const message4 = `名前は${name}です。年齢は${age}です。`
+// console.log(message4);
