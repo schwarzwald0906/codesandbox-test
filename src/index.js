44c3fe4c80066dd503ab2286cbef6f50bf8da829
schwarzwald0906 <93188453+schwarzwald0906@users.noreply.github.com>
2022-07-09 17:10:52 +0900

    型の違い

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
-import "./styles.css";
+// /** */
+// var val1 = "変数";
+// console.log(val1)
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// var val1 = "var変数を再宣言";
+// console.log(val1);
+
+// let val1 = "変数";
+// console.log(val1)
+
+// val1 = "let変数を上書き";
+// console.log(val1);
+
+// var val1 = "let変数を再宣言";
+// console.log(val1);
+
+// const val1 = "const変数";
+// console.log(val1);
+
+// val1 = "const変数を上書き";
+// console.log(val1);
+
+// var val1 = "const変数を再宣言";
+// console.log(val1);
+
+//constで定義したプロパティは変更可能
+// const val4 = {
+//   name: "じゃけえ",
+//   age: 28
+// };
+// console.log(val4.name);
+
+// val4.name = "jak";
+// console.log(val4.name);
+
+// val4.address = "Hiroshima";
+// console.log(val4);
+
+//constで定義した配列は変更可能
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
